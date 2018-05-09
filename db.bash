b5fc20bf0c881404a5a237a072d3ef376da436a3
scottli1203 <scottli1203@sina.com>
2018-05-09 23:49:15 +0800

    初始化仓库

---
+++ db.bash
@@ -0,0 +1,9 @@
+docker run --name dronedb -p 3306:3306 -e MYSQL_ROOT_PASSWORD=abc123_ -d mysql
+
+进入mysql容器：docker exec -it dronedb bash
+
+mysql -u root -p
+
+更改默认的加密方式：ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'root';
+
+GRANT USAGE ON *.* TO 'root'@'%' IDENTIFIED BY PASSWORD '*81F5E21E35407D884A6CD4A731AEBFB6AF209E1B'
