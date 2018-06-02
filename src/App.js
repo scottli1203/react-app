aff0b6eb84ca3ed13fcd106b5105da532c14f693
scottli1203 <scottli1203@sina.com>
2018-06-03 01:09:54 +0800

    初始化仓库

--- src/App.js
+++ src/App.js
@@ -14,6 +14,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload. I am test node.
         </p>
+          <p className="App-intro">
+              To get started, edit <code>src/App.js</code> and save to reload. I am test node.
+          </p>
       </div>
     );
   }
